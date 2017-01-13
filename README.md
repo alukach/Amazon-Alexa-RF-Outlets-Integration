d779eedae193f98cc7b432f53827dfe89cc9e475
Anthony Lukach <anthonylukach@gmail.com>
2017-01-12 21:42:52 -0700

    Migrate code to support RF power outlets

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# LinkNode-R4-Controlled-By-Amazon-Alexa
-This code will create four FauxMo smart-home devices that emulate Belkin We-Mo switches. The unmodified version of this code is originally by kakopappa and can be found at  https://github.com/kakopappa/arduino-esp8266-alexa-multiple-wemo-switch/tree/master/wemos. 
+# RF Power Outlets Controlled By Amazon Alexa
+
+This code will create five FauxMo smart-home devices that emulate Belkin We-Mo switches. This is a third generation fork of code originally by kakopappa and can be found at  https://github.com/kakopappa/arduino-esp8266-alexa-multiple-wemo-switch/tree/master/wemos.
+
+## Parts Used
+
+* [NodeMCU ESP8266 (ESP-12E)](https://www.amazon.com/HiLetgo-Version-NodeMCU-Internet-Development/dp/B010O1G1ES/ref=sr_1_3?ie=UTF8&qid=1484281926&sr=8-3&keywords=esp8266)
+* [Etekcity Wireless Remote Control Electrical Outlet Switch](https://www.amazon.com/Etekcity-Wireless-Electrical-Household-Appliances/dp/B00DQELHBS/ref=sr_1_3?ie=UTF8&qid=1484282201&sr=8-3&keywords=Etekcity)
+* [433Mhz RF Transmitter/Receiver](https://www.amazon.com/UCEC-XY-MK-5V-Transmitter-Receiver-Raspberry/dp/B017AYH5G0/ref=sr_1_2?ie=UTF8&qid=1484282236&sr=8-2&keywords=433mhz+rf+transmitter+and+receiver)
--- wemos.ino
+++ wemos.ino
@@ -1,6 +1,7 @@
 #include <FS.h>                   //this needs to be first, or it all crashes and burns...
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <RCSwitch.h>             //https://github.com/sui77/rc-switch
 #include <WiFiUdp.h>
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
 #include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
@@ -15,6 +16,7 @@ Switch *alexa_switch1 = NULL;
 Switch *alexa_switch2 = NULL;
 Switch *alexa_switch3 = NULL;
 Switch *alexa_switch4 = NULL;
+Switch *alexa_switch5 = NULL;
 
 // Callback prototypes
 void alexa_switch1On();
@@ -25,6 +27,8 @@ void alexa_switch3On();
 void alexa_switch3Off();
 void alexa_switch4On();
 void alexa_switch4Off();
+void alexa_switch5On();
+void alexa_switch5Off();
 
 // Set Relay Pins
 int relayOne = 12;
@@ -33,10 +37,11 @@ int relayThree = 14;
 int relayFour = 16;
 
 // Names each relay/outlet/device is known by -- these are set during config
-char alexa_name1[100] = "";
-char alexa_name2[100] = "";
-char alexa_name3[100] = "";
-char alexa_name4[100] = "";
+char alexa_name1[100] = "1";
+char alexa_name2[100] = "2";
+char alexa_name3[100] = "3";
+char alexa_name4[100] = "4";
+char alexa_name5[100] = "5";
 
 const char* AP_Name = "EchoBase1";
 char saved_ssid[100] = "";
@@ -47,7 +52,22 @@ bool shouldSaveConfig = false;
 bool forceConfigPortal = false;
 
 WiFiManager wifiManager;
-  
+
+// RF Tooling - https://codebender.cc/sketch:80290#RCSwitch%20-%20Transmit%20(Etekcity%20Power%20Outlets).ino
+RCSwitch RFSwitch = RCSwitch();
+int RF_PULSE_LENGTH = 190; // Pulse length to use for RF transmitter
+int RF_TX_PIN = 10; // Digital pin connected to RF transmitter
+int RF_BIT_LENGTH = 24;
+
+// RF Signals (varies per remote controlled plugin set)
+unsigned long rc_codes[5][2] = {
+  // ON     //OFF 
+  {4461875, 4461884}, /* Outlet 1 */
+  {4462019, 4462028}, /* Outlet 2 */
+  {4462339, 4462348}, /* Outlet 3 */
+  {4463875, 4463884}, /* Outlet 4 */
+  {4470019, 4470028}, /* Outlet 5 */
+};
 // Callback notifying us of the need to save config
 void saveConfigCallback () {
     Serial.println("Should save config");
@@ -106,10 +126,12 @@ void setup()
           strcpy(alexa_name2, json["alexa_name2"]);
           strcpy(alexa_name3, json["alexa_name3"]);
           strcpy(alexa_name4, json["alexa_name4"]);
+          strcpy(alexa_name5, json["alexa_name5"]);
           Serial.println("Parsed Alexa relay name #1: " + String(alexa_name1));
           Serial.println("Parsed Alexa relay name #2: " + String(alexa_name2));
           Serial.println("Parsed Alexa relay name #3: " + String(alexa_name3));
           Serial.println("Parsed Alexa relay name #4: " + String(alexa_name4));
+          Serial.println("Parsed Alexa relay name #5: " + String(alexa_name5));
         } else {
           Serial.println("** ERROR ** Failed to load/parse JSON config");
         }
@@ -128,6 +150,7 @@ void setup()
   WiFiManagerParameter custom_alexa_name2("alexa_name2", "Device #2 name", alexa_name2, 100);
   WiFiManagerParameter custom_alexa_name3("alexa_name3", "Device #3 name", alexa_name3, 100);
   WiFiManagerParameter custom_alexa_name4("alexa_name4", "Device #4 name", alexa_name4, 100);
+  WiFiManagerParameter custom_alexa_name5("alexa_name5", "Device #5 name", alexa_name5, 100);
 
   // Set the function that will be called to save the custom parameter after config
   wifiManager.setSaveConfigCallback(saveConfigCallback);
@@ -137,6 +160,7 @@ void setup()
   wifiManager.addParameter(&custom_alexa_name2);
   wifiManager.addParameter(&custom_alexa_name3);
   wifiManager.addParameter(&custom_alexa_name4);
+  wifiManager.addParameter(&custom_alexa_name5);
 
   //reset settings - for testing
   //wifiManager.resetSettings();
@@ -185,10 +209,12 @@ void setup()
   strcpy(alexa_name2, custom_alexa_name2.getValue());
   strcpy(alexa_name3, custom_alexa_name3.getValue());
   strcpy(alexa_name4, custom_alexa_name4.getValue());
+  strcpy(alexa_name5, custom_alexa_name5.getValue());
   Serial.println("Read configured Alexa relay name #1: " + String(alexa_name1));
   Serial.println("Read configured Alexa relay name #2: " + String(alexa_name2));
   Serial.println("Read configured Alexa relay name #3: " + String(alexa_name3));
   Serial.println("Read configured Alexa relay name #4: " + String(alexa_name4));
+  Serial.println("Read configured Alexa relay name #5: " + String(alexa_name5));
           
   // Save the custom parameters to the ESP8266 filesystem if changed
   if (shouldSaveConfig) {
@@ -199,6 +225,7 @@ void setup()
     json["alexa_name2"] = alexa_name2;
     json["alexa_name3"] = alexa_name3;
     json["alexa_name4"] = alexa_name4;
+    json["alexa_name5"] = alexa_name5;
     Serial.println("Attempting to open config JSON file for writing");
     File configFile = SPIFFS.open("/config.json", "w");
     if (!configFile) {
@@ -227,20 +254,18 @@ void setup()
     alexa_switch2 = new Switch(alexa_name2, 81, alexa_switch2On, alexa_switch2Off);
     alexa_switch3 = new Switch(alexa_name3, 82, alexa_switch3On, alexa_switch3Off);
     alexa_switch4 = new Switch(alexa_name4, 83, alexa_switch4On, alexa_switch4Off);
+    alexa_switch5 = new Switch(alexa_name5, 85, alexa_switch5On, alexa_switch5Off);
 
     Serial.println("Adding switches upnp broadcast responder");
     upnpBroadcastResponder.addDevice(*alexa_switch1);
     upnpBroadcastResponder.addDevice(*alexa_switch2);
     upnpBroadcastResponder.addDevice(*alexa_switch3);
     upnpBroadcastResponder.addDevice(*alexa_switch4);
+    upnpBroadcastResponder.addDevice(*alexa_switch5);
 
-    // Set relay pins to outputs
-     pinMode(12,OUTPUT); 
-     pinMode(13,OUTPUT);
-     pinMode(14,OUTPUT);
-     pinMode(16,OUTPUT);
-       
-
+    // Setup RF Transmitter
+    RFSwitch.enableTransmit(RF_TX_PIN);
+    RFSwitch.setPulseLength(RF_PULSE_LENGTH);
 }
 
 /* If disconnected from Wifi, enter a blocking loop that periodically attempts reconnection */
@@ -279,40 +304,76 @@ void loop()
   alexa_switch2->serverLoop();
   alexa_switch3->serverLoop();
   alexa_switch4->serverLoop();
+  alexa_switch5->serverLoop();
 }
 
 void alexa_switch1On() {
-    Serial.print("Switch 1 turn on ...");
-    digitalWrite(relayOne, HIGH);   // sets relayOne on
+    Serial.println("Switch 1 turn on ...");
+    enableOutlet(1, true);
 }
 void alexa_switch1Off() {
-    Serial.print("Switch 1 turn off ...");
-    digitalWrite(relayOne, LOW);   // sets relayOne off
+    Serial.println("Switch 1 turn off ...");
+    enableOutlet(1, false);
 }
 
 void alexa_switch2On() {
-    Serial.print("Switch 2 turn on ...");
-    digitalWrite(relayTwo, HIGH);   // sets relayTwo on
+    Serial.println("Switch 2 turn on ...");
+    enableOutlet(2, true);
 }
 void alexa_switch2Off() {
-    Serial.print("Switch 2 turn off ...");
-    digitalWrite(relayTwo, LOW);   // sets relayTwo off
+    Serial.println("Switch 2 turn off ...");
+    enableOutlet(2, false);
 }
 
 void alexa_switch3On() {
-    Serial.print("Switch 3 turn on ...");
-    digitalWrite(relayThree, HIGH);   // sets relayThree on
+    Serial.println("Switch 3 turn on ...");
+    enableOutlet(3, true);
 }
 void alexa_switch3Off() {
-    Serial.print("Switch 3 turn off ...");
-    digitalWrite(relayThree, LOW);   // sets relayThree off
+    Serial.println("Switch 3 turn off ...");
+    enableOutlet(3, false);
 }
 
 void alexa_switch4On() {
-    Serial.print("Switch 4 turn on ...");
-    digitalWrite(relayFour, HIGH);   // sets relayFour on
+    Serial.println("Switch 4 turn on ...");
+    enableOutlet(4, true);
 }
 void alexa_switch4Off() {
-    Serial.print("Switch 4 turn off ...");
-    digitalWrite(relayFour, LOW);   // sets relayFour off
+    Serial.println("Switch 4 turn off ...");
+    enableOutlet(4, false);
+}
+
+void alexa_switch5On() {
+    Serial.println("Switch 5 turn on ...");
+    enableOutlet(5, true);
+}
+void alexa_switch5Off() {
+    Serial.println("Switch 5 turn off ...");
+    enableOutlet(5, false);
+}
+
+void enableOutlet(int outletNumber, bool onOrOff)
+{
+  if (outletNumber < 1 || outletNumber > 5) {
+    Serial.println("Invalid outlet number");
+    return;
+  }
+  
+  unsigned long *onOffCodes = rc_codes[outletNumber - 1];
+  unsigned long codeToSend = onOffCodes[onOrOff ? 0 : 1];
+  RFSwitch.send(codeToSend, RF_BIT_LENGTH);
+  
+  char outletNumberString[1];
+  int retVal = snprintf(outletNumberString, 1, "%d", outletNumber);
+  if (retVal < 0) {
+    Serial.println("Log encoding error");
+    return;
+  }
+
+  Serial.print("Switch " + String(outletNumber) + " turned ");
+  if (onOrOff) {
+    Serial.println("on");
+  } else {
+    Serial.println("off");
+  }
 }
